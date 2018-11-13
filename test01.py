07692d9b423247bdb7d3a6d6f624c7117bf097ac
zhang.jian <17755900961@163.com>
2018-11-14 01:20:36 +0800

    commit 1

--- test01.py
+++ test01.py
@@ -2,8 +2,9 @@
 # -*- coding=utf-8 -*-
 #本脚由张小只编写，用于Python课程学习
 #人生苦短，我用Python
+#人生苦短，我用Python
+#人生苦短，我用Python
 
 
-if_name_ == '_main_':
-  pass
-    
+print('welcome to qytang1')
+print('welcome to python')
